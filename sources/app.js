e1a3ee99cef2e5e7addebe7d578b30f95fa18dac
Alejandro Torres <atorresalcala.me@gmail.com>
2021-04-17 01:57:18 +0200

    feat(behaviour): navigation control

---
+++ sources/app.js
@@ -0,0 +1,1 @@
+const NavigationApp = new Navigation();
---
+++ sources/components/navigation.js
@@ -0,0 +1,58 @@
+class Navigation {
+	constructor() {
+		this.GUI = {
+			header: document.getElementById('header'),
+			navDesktop: document.getElementById('navDesktop'),
+			navMobile: document.getElementById('navMobile'),
+			hamburger: document.getElementById('hamburger'),
+			close: document.getElementById('close'),
+		};
+		this.initGlobalListeners();
+		this.initListeners();
+	}
+
+	initGlobalListeners = () => {
+		const rtAnimationFrame =
+			window.requestAnimationFrame ||
+			window.mozRequestAnimationFrame ||
+			window.webkitRequestAnimationFrame ||
+			window.msRequestAnimationFrame;
+		window.requestAnimationFrame = rtAnimationFrame;
+
+		document.addEventListener('visibilitychange', () => {
+			document.visibilityState == 'hidden'
+				? this.closeListeners()
+				: this.initListeners();
+		});
+	};
+
+	initListeners = () => {
+		document.title = 'Frontend Mentor | Bookmark landing page';
+		this.GUI.hamburger.addEventListener('click', this._openMobileNav);
+		this.GUI.close.addEventListener('click', this._closeMobileNav);
+	};
+
+	closeListeners = () => {
+		document.title = 'Eh! Come back please :(';
+		this.GUI.hamburger.removeEventListener('click', this._openMobileNav);
+		this.GUI.close.removeEventListener('click', this._closeMobileNav);
+	};
+
+	_openMobileNav = () => {
+		document.body.style.height = '100vh';
+		document.body.style.overflowY = 'hidden';
+		this.GUI.header.style.visibility = 'hidden';
+		this.GUI.navMobile.style.display = 'block';
+		window.requestAnimationFrame(() => (this.GUI.navMobile.style.left = 0));
+	};
+
+	_closeMobileNav = () => {
+		document.body.style.height = 'auto';
+		document.body.style.overflowY = 'auto';
+		this.GUI.header.style.visibility = 'visible';
+		this.GUI.navMobile.style.display = 'none';
+		window.requestAnimationFrame(
+			() => (this.GUI.navMobile.style.left = '-100%')
+		);
+	};
+}
